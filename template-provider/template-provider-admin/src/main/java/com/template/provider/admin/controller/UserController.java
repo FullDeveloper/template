618cfebe73ba029f952f244f6271d51e835c7c36
ZhouRunBin <1875222156@qq.com>
2018-05-31 13:08:32 +0800

    菜单路由调整

--- template-provider/template-provider-admin/src/main/java/com/template/provider/admin/controller/UserController.java
+++ template-provider/template-provider-admin/src/main/java/com/template/provider/admin/controller/UserController.java
@@ -1,21 +1,22 @@
 package com.template.provider.admin.controller;
 
 import com.template.auth.client.config.UserAuthConfig;
+import com.template.auth.common.bean.PermissionInfo;
 import com.template.auth.common.util.JWTHelper;
 import com.template.common.context.BaseContextHandler;
 import com.template.common.controller.BaseController;
 import com.template.common.result.ObjectRestResponse;
 import com.template.provider.admin.biz.UserBiz;
 import com.template.provider.admin.entity.User;
+import com.template.provider.admin.rpc.service.PermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -30,6 +31,8 @@ import java.util.Objects;
 @RequestMapping("user")
 public class UserController extends BaseController<UserBiz, User> {
 
+    @Autowired
+    private PermissionService permissionService;
 
     @RequestMapping(value = "/info", method = RequestMethod.GET)
     public ObjectRestResponse<User> getUserInfoByToken() throws Exception {
@@ -38,14 +41,20 @@ public class UserController extends BaseController<UserBiz, User> {
     }
 
     @RequestMapping(value = "/newUserInsert", method = RequestMethod.POST)
-    public ObjectRestResponse newUserInsert(@Validated @RequestBody  User user, BindingResult result) {
+    public ObjectRestResponse newUserInsert(@Validated @RequestBody User user, BindingResult result) {
         ObjectRestResponse restResponse = new ObjectRestResponse();
-        if(result.hasErrors()){
+        if (result.hasErrors()) {
             restResponse.setStatus(400);
             restResponse.setMessage(Objects.requireNonNull(result.getFieldError()).getDefaultMessage());
             return restResponse;
         }
         return baseBiz.newUserInsert(user);
+    }
+
+    @RequestMapping(value = "/un/{username}/resources", method = RequestMethod.GET)
+    @ResponseBody
+    public ObjectRestResponse getPermissionByUsername(@PathVariable("username") String username) {
+        return baseBiz.getPermissionByUsername(username);
     }
 
 }
